243955b2d69bb30b27c4a34ab909ef1f77d542d2
avidnerd <subhisuper@gmail.com>
2026-01-04 13:57:42 -0500

    move up frame

--- src/components/Hobbies.jsx
+++ src/components/Hobbies.jsx
@@ -335,7 +335,7 @@ const ArtGallery = () => {
       <p className="text-secondary text-[17px] mb-8">
         Click on the artwork to see the next piece
       </p>
-      <div className="relative max-w-4xl mx-auto" style={{ transform: 'scale(1.2)' }}>
+      <div className="relative max-w-4xl mx-auto -mt-8" style={{ transform: 'scale(1.2)' }}>
         {/* Single frame container */}
         <div className="relative w-full aspect-square cursor-pointer" onClick={nextArt}>
           {/* Frame - base layer */}
